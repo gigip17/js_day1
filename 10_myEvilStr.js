4313e49bb168b9322d961ae0f82a439bec8836f2
Pierre <pierre@MacBook-Air-de-Pierre.local>
2023-01-03 10:05:57 +0100

    exo--11

---
+++ 10_myEvilStr.js
@@ -0,0 +1,4 @@
+function myEvilStr(str) {
+    return str.split('').reverse().join('');
+  }
+  module.exports = myEvilStr;
---
+++ 11_myStrCat.js
@@ -0,0 +1,4 @@
+function myStrCat(str1, str2) {
+    return str1 + str2;
+  }
+  module.exports = myStrCat;
