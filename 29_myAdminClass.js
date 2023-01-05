80af2322fa0276f6f168db8755d33a5f8f5ba1bb
gigip17 <pierre@MacBook-Air-de-Pierre.local>
2023-01-05 16:26:31 +0100

    29

---
+++ 29_myAdminClass.js
@@ -0,0 +1,22 @@
+class User{
+    constructor(surname,name,age){
+        this.surname = surname;
+        this.name = name;
+        this.age = age;
+    }
+    presentation(){
+        return (`Je m'apelle ${this.surname} ${this.name}, j'ai ${this.age} ans`)
+     }
+}
+class Admin extends User{
+    constructor(surname, name, age) {
+        super(surname, name, age);
+        this.role = "administrateur";
+    }
+}
+
+
+
+let me = new Admin("Guilian", "Ganster", 22);
+console.log(me.presentation()); //"Je m'apelle Guilian Ganster, j'ai 22 ans"
+console.log(me.role); //"administrateur" 
